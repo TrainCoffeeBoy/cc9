30a026941b0d93ad65d6edde0680a4dcd294c767
{TrainCoffeeBoy} <{oumeitaro@gmail.com}>
2020-09-11 22:44:54 +0900

    hundling unary + and -

--- 9cc.c
+++ 9cc.c
@@ -161,17 +161,26 @@ Node	*primary(void)
 	return new_node_num(expect_number());
 }
 
+Node	*unary(void)
+{
+	if (consume('+'))
+		return (primary());
+	if (consume('-'))
+		return (new_node(ND_SUB, new_node_num(0), primary()));
+	return (primary());
+}
+
 Node	*mul(void)
 {
 	Node *node;
 
-	node = primary();
+	node = unary();
 	while (1)
 	{
 		if (consume('*'))
-			node = new_node(ND_MUL, node, primary());
+			node = new_node(ND_MUL, node, unary());
 		else if (consume('/'))
-			node = new_node(ND_DIV, node, primary());
+			node = new_node(ND_DIV, node, unary());
 		else
 			return (node);
 	}
--- test.sh
+++ test.sh
@@ -23,5 +23,8 @@ assert 41 " 12 + 34 - 5 "
 assert 47 '5+6*7'
 assert 15 '5*(9-6)'
 assert 4 '(3+5)/2'
+assert 10 '-3 + 13'
+assert 10 '+3 + (4 + 3)'
+assert 10 '-(3 + 10) + 23'
 
 echo OK
